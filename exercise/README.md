ab699ec43896b34274741ae2a904f48096db9ece
HeartLezz7 <heartchantapath@gmail.com>
2024-12-01 17:45:08 +0700

    create exam of use effect

---
+++ exercise/README.md
@@ -0,0 +1,2 @@
+lab1
+1.ให้ลองเปิด console.log
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+
 function App() {
+  //########## lab 1 ##########
+  console.log("App");
+  useEffect(() => {
+    console.log("useEffect Active");
+
+    return console.log("Clean up effect active");
+  }, []);
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
